6ec16d974e9eaa9c220331e8d979dec5e8596845
a00783576 <arturo-gutierrez@live.com>
2019-08-01 11:44:03 -0500

    Added cucumber tests

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 test $(curl docker.for.win.localhost:8765/sum?a=1"&"b=2) -eq 3
+./gradlew acceptanceTest -Dcalculator.url=docker.for.win.localhost:8765
---
+++ src/test/java/acceptance/AcceptanceTest.java
@@ -0,0 +1,12 @@
+package acceptance;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/** Acceptance Test */
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:feature")
+public class AcceptanceTest {
+    
+}
