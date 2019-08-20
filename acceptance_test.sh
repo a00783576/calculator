41272e77b2400445a3211537cce8f96e2f787403
a00783576 <arturo-gutierrez@live.com>
2019-08-20 13:28:10 -0500

    Update acceptance_test.sh

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #./gradlew acceptanceTest -Dcalculator.url=http://localhost:8081
-./gradlew acceptanceTest -Dcalculator.url=http://host.docker.internal:8765
+./gradlew acceptanceTest -Dcalculator.url=http://docker.for.win.localhost:8765
